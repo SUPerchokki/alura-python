3f94679a0eb8a04019343fec37376a476d561498
SUPerchokki <32721115+SUPerchokki@users.noreply.github.com>
2022-08-28 12:26:09 -0300

    plotando graficos

--- curso1/imports.py
+++ curso1/imports.py
@@ -1,4 +1,6 @@
 from random import randrange
+import matplotlib.pyplot as plt
+
 
 notas = []
 
@@ -6,3 +8,10 @@ for i in range(8):
     notas.append(randrange(0,11))
 
 print(notas)
+
+x = list(range(1, 9))
+y = notas
+
+plt.plot(x,y)
+plt.title('Notas de matematica')
+plt.show()
